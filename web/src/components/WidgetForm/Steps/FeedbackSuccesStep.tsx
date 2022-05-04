dc6df64e13f5a5912f438adbddf716372e0b1cca
Gabriel Peruchi <gabriel-peruchi@hotmail.com>
2022-05-04 11:08:15 -0300

    feat(web): widget form

---
+++ web/src/components/WidgetForm/Steps/FeedbackSuccesStep.tsx
@@ -0,0 +1,33 @@
+import { CloseButton } from "../../CloseButton";
+
+import successImageUrl from '../../../assets/success.svg'
+
+type FeedbackSuccesStepProps = {
+  onFeedbackRestartRequested: () => void
+}
+
+export function FeedbackSuccesStep({ onFeedbackRestartRequested }: FeedbackSuccesStepProps) {
+  return (
+    <>
+      <header>
+        <CloseButton />
+      </header>
+
+      <div className="flex flex-col items-center py-10 w-[304px]">
+        <img src={successImageUrl} />
+
+        <span className="text-xl mt-2">
+          Agradecemos o feedback!
+        </span>
+
+        <button
+          type="button"
+          onClick={onFeedbackRestartRequested}
+          className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
+        >
+          Quero enviar outro
+        </button>
+      </div>
+    </>
+  )
+}
