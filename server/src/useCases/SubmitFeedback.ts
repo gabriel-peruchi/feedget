85f3f1a22a581daaa564126da6fb0ebee02603db
Gabriel Peruchi <gabriel-peruchi@hotmail.com>
2022-05-06 22:50:16 -0300

    feat(server): screenshot in the body of the feedback submission email

--- server/src/useCases/SubmitFeedback.ts
+++ server/src/useCases/SubmitFeedback.ts
@@ -36,16 +36,22 @@ export class SubmitFeedback {
       screenshot
     })
 
+    const emailBody = [
+      '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
+        `<p>Tipo do feedback: ${type}</p>`,
+        `<p>Comentário: ${comment}</p>`,
+      '</div>'
+    ]
+
+    if (screenshot) {
+      emailBody.push(`<img src="${screenshot}" />`,)
+    }
+
     await this.mailProvider.sendMail({
       from: 'Equipe Feedget <oi@feedget.com>',
       to: 'Gabriel Peruchi <gabriel-peruchi@hotmail.com>',
       subject: 'Novo feedback',
-      body: [
-        '<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
-        `<p>Tipo do feedback: ${type}</p>`,
-        `<p>Comentário: ${comment}</p>`,
-        '</div>'
-      ].join('\n')
+      body: emailBody.join('\n')
     })
   }
 }
